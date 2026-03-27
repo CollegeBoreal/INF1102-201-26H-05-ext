3dd2af8376b1ae1eae29bde1970f2382b1babd6c
ismailtrache <ismailtrache2@gmail.com>
2026-03-27 13:50:33 +0000

    feat: script_gestion.sh avec /entreprise/ racine + logs + cron

--- 5.BATCH/300150395/script_gestion.sh
+++ 5.BATCH/300150395/script_gestion.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-LOG=~/entreprise/logs/log.txt
+LOG=/entreprise/logs/log.txt
 DATE=$(date)
+
+# S'assurer que les dossiers existent
+mkdir -p /entreprise/logs
+mkdir -p /entreprise/backup
 
 echo "===================================" >> $LOG
 echo "Début exécution : $DATE" >> $LOG
@@ -12,7 +16,7 @@ ping -c 4 8.8.8.8 >> $LOG 2>&1
 
 # 2. Sauvegarde des fichiers
 echo "Sauvegarde en cours..." >> $LOG
-cp -r ~/entreprise/data/* ~/entreprise/backup/ >> $LOG 2>&1
+cp -r /entreprise/data/* /entreprise/backup/ >> $LOG 2>&1
 
 # 3. Création utilisateur temporaire
 USER_TEMP="employe_temp"
@@ -20,13 +24,13 @@ USER_TEMP="employe_temp"
 if id "$USER_TEMP" &>/dev/null; then
     echo "Utilisateur existe déjà." >> $LOG
 else
-    sudo useradd $USER_TEMP
-    echo "$USER_TEMP:Temp1234" | sudo chpasswd
+    useradd $USER_TEMP
+    echo "$USER_TEMP:Temp1234" | chpasswd
     echo "Utilisateur créé." >> $LOG
 fi
 
 # 4. Compression archive
-tar -czvf ~/entreprise/backup/backup_$(date +%F).tar.gz ~/entreprise/data >> $LOG 2>&1
+tar -czvf /entreprise/backup/backup_$(date +%F).tar.gz /entreprise/data >> $LOG 2>&1
 
 echo "Fin exécution : $(date)" >> $LOG
 echo "===================================" >> $LOG
