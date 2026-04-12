4fa6d3e45b92ce51844b40fdbac2739e68e73991
Adel <adel@gmail.com>
2026-04-12 13:56:58 +0000

    Ajout scripts REGEX - 300151466

--- 7.REGEX/300151466/analyse_nginx.ps1
+++ 7.REGEX/300151466/analyse_nginx.ps1
@@ -1,1 +1,16 @@
+$log = Get-Content /var/log/nginx/access.log
+$ips = $log | ForEach-Object {
+    if ($_ -match '^(\d+\.\d+\.\d+\.\d+)') {
+        $matches[1]
+    }
+}
 
+$result = $ips | Group-Object | Sort-Object Count -Descending | Select-Object -First 10
+
+$output = "REGEX/rapport_nginx_ps1_2026-03-31.txt"
+
+$result | ForEach-Object {
+    "$($_.Name) - $($_.Count)" | Out-File -Append $output
+}
+
+Write-Host "Rapport généré : $output"
