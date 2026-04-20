1fe9ea6782808d111e3255f8a8d6ff3846b0e9a4
Kahina <kahinanina9555@gmail.com>
2026-04-20 21:12:28 +0000

    Ajout projet Bash + Python

---
+++ 8.Project/300151556/scripts/analyse.py
@@ -0,0 +1,28 @@
+import sys
+from collections import Counter
+
+file = sys.argv[1]
+
+ips = []
+urls = []
+
+with open(file, 'r') as f:
+    for line in f:
+        parts = line.split()
+        ips.append(parts[0])
+        urls.append(parts[5])
+
+top_ips = Counter(ips).most_common(3)
+top_urls = Counter(urls).most_common(3)
+
+with open("output/rapport.txt", "w") as out:
+    out.write("Top IPs:\n")
+    for ip, count in top_ips:
+        out.write(f"{ip} : {count}\n")
+
+    out.write("\nTop URLs:\n")
+    for url, count in top_urls:
+        out.write(f"{url} : {count}\n")
+
+print("Rapport généré dans output/rapport.txt")
+
---
+++ 8.Project/300151556/scripts/analyse.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Analyse du fichier log..."
+
+python3 scripts/analyse.py data/sample.log
+
+echo "Terminé. Voir output/rapport.txt"
+
