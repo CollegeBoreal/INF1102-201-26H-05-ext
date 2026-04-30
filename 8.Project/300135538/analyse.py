f1dcaa5b02575b9a685b162f5f8bfb93f2febdac
Reda <duliquide2@gmail.com>
2026-04-30 16:02:07 -0400

    Fix rapport.txt location

--- 8.Project/300135538/analyse.py
+++ 8.Project/300135538/analyse.py
@@ -13,7 +13,7 @@ with open("data/sample.log", "r") as fichier:
 top_ips = Counter(ips).most_common(3)
 top_urls = Counter(urls).most_common(3)
 
-with open("output/rapport.txt", "w") as rapport:
+with open("rapport.txt", "w") as rapport:
     rapport.write("===== RAPPORT ANALYSE LOG =====\n\n")
 
     rapport.write("Top IP:\n")
